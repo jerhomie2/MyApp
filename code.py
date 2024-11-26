9a61e2421b5ebc56fd4fb373658812f850a758ce
Jarom Asher <jaromasher@gmail.com>
2024-11-26 14:38:27 -0700

    sidebar

--- code.py
+++ code.py
@@ -15,6 +15,15 @@ with st.sidebar:
     input_age = st.slider('Age Range', min_value=27, max_value=59, value=(40,50))
     input_gender = st.radio('Gender', ['Male','Female'])
     input_disorder = st.radio('Disorder', ['Sleep Apnea','Insomnia', 'None '])
+    male_button = st.button('Male')
+    female_button = st.button('Female')
+    if male_button:
+        input_gender = 'Male'
+    elif female_button:
+        input_gender = 'Female'
+    else:
+        input_gender = None  # No gender selected yet
+    st.write(f"Selected Gender: {input_gender}")
 
 tab1, tab2, tab3 = st.tabs(['Age','Gender','Sleep Disorder'])
 with tab1:
@@ -26,7 +35,7 @@ with tab1:
 with tab2:
     df1 = df[df['Gender'] == input_gender].copy()
     avg_sleep_duration = df1.groupby(['Gender','Age', 'BMI Category'])['Sleep Duration'].mean().reset_index()
-    fig1 = px.line(avg_sleep_duration, x='Age', y='Sleep Duration', title='Average Sleep Duration by Gender', color = 'BMI Category')
+    fig1 = px.line(avg_sleep_duration, x='Age', y='Sleep Duration', title='Average Sleep Duration by Gender and BMI Category', color = 'BMI Category')
     st.plotly_chart(fig1)
 
 with tab3:
