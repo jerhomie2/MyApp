4c531df5ad59ed789fd74912ee3b74cfb4388010
Jarom Asher <jaromasher@gmail.com>
2024-11-26 13:12:23 -0700

    tab2

--- code.py
+++ code.py
@@ -11,20 +11,20 @@ df = pd.read_csv('sleep.csv')
 
 st.title('Sleep Data')
 
-# tab1, tab2 = st.tabs(['Age','Gender'])
-# with tab1:
-#     input_age = st.slider('Age', min_value=27, max_value=59, value=40)
-#     df1 = df[df['Age'] == input_age].copy()
-#     average_sleep_duration = df1['Sleep Duration'].mean()
-#     bar_data = pd.DataFrame({
-#         'Age': [input_age],
-#         'Average Sleep Duration': [average_sleep_duration]
-#     })
-#     fig1 = px.bar(df, x='Age', y='Average Sleep Duration', title='Average Sleep Duration by Age')
-#     st.plotly_chart(fig1)
+tab1, tab2 = st.tabs(['Age','Gender'])
+with tab1:
+    input_age = st.slider('Age', min_value=27, max_value=59, value=40)
+    df1 = df[df['Age'] == input_age].copy()
+    average_sleep_duration = df1['Sleep Duration'].mean()
+    bar_data = pd.DataFrame({
+        'Age': [input_age],
+        'Average Sleep Duration': [average_sleep_duration]
+    })
+    fig1 = px.bar(df, x='Age', y='Average Sleep Duration', title='Average Sleep Duration by Age')
+    st.plotly_chart(fig1)
 
-# with tab2:
-#     st.header("Demographic Overview")
-#     gender_count = df['Gender'].value_counts()
-#     fig2 = px.pie(names=gender_count.index, values=gender_count.values, title="Gender Distribution")
-#     st.plotly_chart(fig2)
+with tab2:
+    st.header("Demographic Overview")
+    gender_count = df['Gender'].value_counts()
+    fig2 = px.pie(names=gender_count.index, values=gender_count.values, title="Gender Distribution")
+    st.plotly_chart(fig2)
