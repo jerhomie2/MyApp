64778d2e5b54f9ae0a155a77b95f0e8fedd82988
Jarom Asher <jaromasher@gmail.com>
2024-11-26 14:31:52 -0700

    tab2

--- code.py
+++ code.py
@@ -16,19 +16,19 @@ with tab1:
     input_age = st.slider('Age Range', min_value=27, max_value=59, value=(40,50))
     df1 = df[(df['Age'] >= input_age[0]) & (df['Age'] <= input_age[1])].copy()
     avg_sleep_duration = df1.groupby('Age')['Sleep Duration'].mean().reset_index()
-    fig1 = px.bar(avg_sleep_duration, x='Age', y='Sleep Duration', title='Sleep Duration by Age')
+    fig1 = px.bar(avg_sleep_duration, x='Age', y='Sleep Duration', title='Average Sleep Duration by Age')
     st.plotly_chart(fig1)
 
 with tab2:
     input_gender = st.radio('Gender', ['Male','Female'])
     df1 = df[df['Gender'] == input_gender].copy()
     avg_sleep_duration = df1.groupby(['Gender','Age', 'BMI Category'])['Sleep Duration'].mean().reset_index()
-    fig1 = px.bar(avg_sleep_duration, x='Age', y='Avg Sleep Duration', title='Sleep Duration by Gender', color = 'BMI Category', barmode='group')
+    fig1 = px.bar(avg_sleep_duration, x='Age', y='Sleep Duration', title='Average Sleep Duration by Gender', color = 'BMI Category', barmode='group')
     st.plotly_chart(fig1)
 
 with tab3:
     input_disorder = st.radio('Disorder', ['Sleep Apnea','Insomnia', 'None '])
     df1 = df[df['Sleep Disorder'] == input_disorder].copy()
     avg_sleep_duration = df1.groupby(['Sleep Disorder','Age', 'Gender'])['Sleep Duration'].mean().reset_index()
-    fig1 = px.bar(avg_sleep_duration, x='Age', y='Avg Sleep Duration', title='Sleep Duration by Disorder', color = 'Gender', barmode='group')
+    fig1 = px.bar(avg_sleep_duration, x='Age', y='Sleep Duration', title='Average Sleep Duration by Disorder', color = 'Gender', barmode='group')
     st.plotly_chart(fig1)
